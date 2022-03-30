5e58492b400143844ca3c836556c93af5064b87d
HAOUDER Oussama <OHAOUDER@micropole.com>
2022-03-30 16:44:00 +0200

    Fix Alt error

--- src/components/Technos/TechnoList.jsx
+++ src/components/Technos/TechnoList.jsx
@@ -11,8 +11,8 @@ const TechnoList = ()=>{
 			<span class="nucleo"></span>
 		 </span>
         </div>
-			<span > <img src= "https://i.ibb.co/P4Gk6gf/angular.png"></img></span>
-            <span className="test"><img src= "https://i.ibb.co/9c6MYbp/spring-boot-logo.png "></img></span>
+			<span > <img src= "https://i.ibb.co/P4Gk6gf/angular.png" alt=""></img></span>
+            <span className="test"><img src= "https://i.ibb.co/9c6MYbp/spring-boot-logo.png " alt=""></img></span>
 
         </div>     
     </div>
