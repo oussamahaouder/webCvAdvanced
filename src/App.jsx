49458b7c64de1cceea3c2ac5013e4a24e6208a00
HAOUDER Oussama <OHAOUDER@micropole.com>
2022-03-30 16:25:38 +0200

    [UpdatedLast=> Adding New Part , fixing issues ]

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import ProductList from "./components/productList/ProductList";
 import Toggle from "./components/toggle/Toggle";
 import { ThemeContext } from "./context";
 import{ init } from '@emailjs/browser';
+import TechnoList from "./components/Technos/TechnoList";
 
 init("user_v7MOGb64xlRhwyQUr2cO1");
 const App = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
+    <>
     <div
       style={{
         backgroundColor: darkMode ? "#222" : "white",
@@ -22,9 +24,11 @@ const App = () => {
       <Intro />
       <About />
       <ProductList />
+      <TechnoList/>
       <Contact />
     </div>
-  );
+    
+    </>  );
 };
 
 export default App;
---
+++ src/components/Technos/TechnoList.jsx
@@ -0,0 +1,26 @@
+import "./TechnoList.css"
+const TechnoList = ()=>{
+    return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Technologies</h1>
+      </div>
+      <div className="pl-list ">
+        <div className="p">
+         <span class="react-logo">
+			<span class="nucleo"></span>
+		 </span>
+        </div>
+			<span > <img src= "https://i.ibb.co/P4Gk6gf/angular.png"></img></span>
+            <span className="test"><img src= "https://i.ibb.co/9c6MYbp/spring-boot-logo.png "></img></span>
+
+        </div>     
+    </div>
+      
+      
+      
+    )
+
+}
+
+export default TechnoList ; 
